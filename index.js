2f1dedeac788b44c7305c6635274d137d3f11027
Ahmed-dagger <143612205+Ahmed-dagger@users.noreply.github.com>
2023-08-30 15:41:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function menu() 
+    {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") 
+      {
+        x.style.display = "none";
+      } 
+      else
+      {
+        x.style.display = "block";
+      }
+    }
+
+    function drop()
+    {
+      var x = document.getElementById("dropdown");
+      if (x.style.display === "block") 
+      {
+        x.style.display = "none";
+      } 
+      else
+      {
+        x.style.display = "block";
+      }
+    }
+
+    /*
+
+    function search()
+    {
+      var x = document.getElementById("searchbox");
+      if (x.style.display === "block") 
+      {
+        x.style.display = "none";
+      } 
+      else
+      {
+        x.style.display = "block";
+      }
+    }
+    */
